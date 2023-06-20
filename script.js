23a1bd2be2e7eaf76c478dc85386ff7d9a3e1e38
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 14:01:21 -0300

    Criação de upload e vizualização

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeInsert = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
 
-function textChange() {
-    memeText.innerText = textInput.value;
+function changeText() {
+  memeText.innerText = textInput.value;
 }
 
-textInput.addEventListener('input', textChange);
+function uploadImage(event) {
+  const file = event.target.files[0];
+  if (file) {
+    memeImage.src = URL.createObjectURL(file);
+  }
+}
+
+
+textInput.addEventListener('input', changeText);
+memeInsert.addEventListener('change', uploadImage);
