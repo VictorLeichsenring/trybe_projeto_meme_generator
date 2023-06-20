5a3e1652f2926d109186009aa01e40b65f7cfffa
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 14:36:41 -0300

    Criação de botões fire, water, earth

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const memeInsert = document.getElementById('meme-insert');
+const memeContainer = document.getElementById('meme-image-container');
 const memeImage = document.getElementById('meme-image');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
 
 function changeText() {
   memeText.innerText = textInput.value;
@@ -14,6 +18,20 @@ function uploadImage(event) {
   }
 }
 
+function fireButton() {
+  memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
+}
+
+function waterButton() {
+  memeContainer.style.border = '5px double rgb(0, 0, 255)';
+}
+
+function earthButton() {
+  memeContainer.style.border = '6px groove rgb(0, 128, 0)';
+}
 
 textInput.addEventListener('input', changeText);
 memeInsert.addEventListener('change', uploadImage);
+btnFire.addEventListener('click', fireButton);
+btnWater.addEventListener('click', waterButton);
+btnEarth.addEventListener('click', earthButton);
