ef79a2bd7dd7e97c367a606cfd11c55154caec81
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 15:02:49 -0300

    Criação de miniaturas de meme sugeridas

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const memeImage = document.getElementById('meme-image');
 const btnFire = document.getElementById('fire');
 const btnWater = document.getElementById('water');
 const btnEarth = document.getElementById('earth');
+const minimeme1 = document.getElementById('meme-1');
+const minimeme2 = document.getElementById('meme-2');
+const minimeme3 = document.getElementById('meme-3');
+const minimeme4 = document.getElementById('meme-4');
 
 function changeText() {
   memeText.innerText = textInput.value;
@@ -30,8 +34,17 @@ function earthButton() {
   memeContainer.style.border = '6px groove rgb(0, 128, 0)';
 }
 
+function minimeme(event) {
+  const img = event.target.src;
+  memeImage.src = img;
+}
+
 textInput.addEventListener('input', changeText);
 memeInsert.addEventListener('change', uploadImage);
 btnFire.addEventListener('click', fireButton);
 btnWater.addEventListener('click', waterButton);
 btnEarth.addEventListener('click', earthButton);
+minimeme1.addEventListener('click', minimeme);
+minimeme2.addEventListener('click', minimeme);
+minimeme3.addEventListener('click', minimeme);
+minimeme4.addEventListener('click', minimeme);
